c8f0710ff905185051b1df8e4152f20980c1faa1
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-04-05 15:15:43 -0400

    php created

---
+++ labtest4/labtest_stored.php
@@ -0,0 +1,4 @@
+<i style="color:blue;font-size:30px;font-family:calibri ;">
+      FATIH KUCUKGOKMEN </i> <i style="color:green;font-size:30px;font-family:calibri ;">
+      FATIH KUCUKGOKMEN </i> <i style="color:red;font-size:30px;font-family:calibri ;">
+      FATIH KUCUKGOKMEN </i> 
